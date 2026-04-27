1fe58842ac59665d77c63ed17a0556f34be93841
tkevinjd <3160494497@qq.com>
2026-04-27 14:13:29 +0800

    feat:新增错误码定义与当前登录用户上下文类的定义

---
+++ framework/src/main/java/com/tkevinjd/framework/errorcode/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.tkevinjd.framework.errorcode;
+
+/**
+ * 定义错误码接口
+ */
+public interface ErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
